6f80045d445c43896a99c67dc2c5d167ae299a55
coletownsend <cole@coletownsend.com>
2021-02-09 21:06:57 -0500

    update it all

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import App from 'next/app';
 import { ThemeProvider, Container } from 'theme-ui';
 import NProgress from 'next-nprogress-emotion';
+import { DefaultSeo } from 'next-seo';
 
 import Header from '../components/Header';
 import theme from '../theme';
@@ -13,6 +14,20 @@ class MyApp extends App {
         <Header />
         <NProgress />
         <Container>
+          <DefaultSeo
+            openGraph={{
+              type: 'website',
+              locale: 'en_US',
+              url: 'https://shoekicker.com',
+              site_name: 'Find the internet’s best prices on running shoes',
+            }}
+            canonical="https://shoekicker.com"
+            twitter={{
+              handle: '@shoe_kicker',
+              site: '@shoe_kicker',
+              cardType: 'summary_large_image',
+            }}
+          />
           <Component {...pageProps} />
         </Container>
       </ThemeProvider>
